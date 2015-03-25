e76f5347e8f5805bc9d528a19455c0e28ba323c4
Félix Darricau <felix.darricau@gmail.com>
2015-03-25 11:09:12 +0900

    Cleaner error checking in DescriptiveWrapper

--- include/roboptim/core/filter/manifold-map/descriptive-wrapper.hxx
+++ include/roboptim/core/filter/manifold-map/descriptive-wrapper.hxx
@@ -13,42 +13,15 @@ namespace roboptim
     : fct_ (fct),
       manifold_ (&functionManifold)
   {
-    if (manifold_->isElementary())
+    long size = manifold_->representationDim();
+    if (fct_->inputSize() != size)
     {
-      if (fct_->inputSize() != manifold_->representationDim())
-      {
-        std::stringstream* error = new std::stringstream;
-        (*error) << "Representation dims mismatch on Elementary manifold "
-                 << manifold_->name() << " using function "
-                 << fct->getName() << ". Expected dimension :"
-                 << manifold_->representationDim() << ", actual one is "
-                 << fct_->inputSize();
-        throw std::runtime_error (error->str());
-      }
-    }
-    else
-    {
-      long size = 0;
-      size_t manifolds = manifold_->numberOfSubmanifolds();
-      for (size_t i = 0; i < manifolds; ++i)
-        size += ((*manifold_) (i)).representationDim();
-      if (manifold_->representationDim() != size)
-      {
-        std::stringstream* error = new std::stringstream;
-        (*error) << "Invalid Composed Manifold " << manifold_->name()
-                 << ". Size equals " << manifold_->representationDim()
-                 << " whereas the sum of submanifold's equals " << size;
-        throw std::runtime_error (error->str());
-      }
-      if (fct_->inputSize() != size)
-      {
-        std::stringstream* error = new std::stringstream;
-        (*error) << "Representation dims mismatch on Composed manifold "
-                 << manifold_->name() << " using function "
-                 << fct->getName() << ". Expected dimension :" << size
-                 << ", actual one is " << fct_->inputSize();
-        throw std::runtime_error (error->str());
-      }
+      std::stringstream* error = new std::stringstream;
+      (*error) << "Representation dims mismatch on manifold "
+               << manifold_->name() << " using function "
+               << fct->getName() << ". Expected dimension :" << size
+               << ", actual one is " << fct_->inputSize();
+      throw std::runtime_error (error->str());
     }
   }
 
